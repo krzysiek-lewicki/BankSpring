472b0e841a5370db6e6b627c5139210ceaee7433
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-07-20 11:19:23 +0200

    Base-xml

--- src/main/java/pl/training/bank/App.java
+++ src/main/java/pl/training/bank/App.java
@@ -1,5 +1,6 @@
 package pl.training.bank;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.training.bank.entity.Account;
 import pl.training.bank.operation.DepositOperation;
 import pl.training.bank.operation.TransferOperation;
@@ -13,20 +14,22 @@ import pl.training.bank.service.repository.HashMapAccountsRepository;
 public class App {
 
     public static void main(String[] args) {
-        AccountNumberGenerator accountNumberGenerator = new IncrementalAccountNumberGenerator();
-        AccountsRepository accountsRepository = new HashMapAccountsRepository();
-        AccountsService accountsService = new AccountsService(accountsRepository, accountNumberGenerator);
 
-        Account firstAccount = accountsService.createAccount();
-        Account secondAccount = accountsService.createAccount();
+        try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml")) {
 
-        accountsService.process(new DepositOperation(firstAccount.getNumber(), 1000));
-        accountsService.process(new DepositOperation(secondAccount.getNumber(), 500));
-        accountsService.process(new WithdrawOperation(firstAccount.getNumber(), 200));
-        accountsService.process(new TransferOperation(firstAccount.getNumber(), secondAccount.getNumber(), 10));
+            AccountsService accountsService = applicationContext.getBean(AccountsService.class);
 
-        System.out.println(firstAccount);
-        System.out.println(secondAccount);
+            Account firstAccount = accountsService.createAccount();
+            Account secondAccount = accountsService.createAccount();
+
+            accountsService.process(new DepositOperation(firstAccount.getNumber(), 1000));
+            accountsService.process(new DepositOperation(secondAccount.getNumber(), 500));
+            accountsService.process(new WithdrawOperation(firstAccount.getNumber(), 200));
+            accountsService.process(new TransferOperation(firstAccount.getNumber(), secondAccount.getNumber(), 10));
+
+            System.out.println(firstAccount);
+            System.out.println(secondAccount);
+        }
     }
 
 }
