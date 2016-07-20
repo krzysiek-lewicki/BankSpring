29f230dc2dd416a21ea050b1b6ba287e3984994a
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-07-20 11:42:45 +0200

    Base-annotations

--- src/main/java/pl/training/bank/service/AccountsService.java
+++ src/main/java/pl/training/bank/service/AccountsService.java
@@ -1,18 +1,28 @@
 package pl.training.bank.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 import pl.training.bank.entity.Account;
 import pl.training.bank.operation.Operation;
 import pl.training.bank.service.repository.AccountsRepository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Service
 public class AccountsService {
 
     private AccountsRepository accountsRepository;
     private AccountNumberGenerator accountNumberGenerator;
 
-    public AccountsService(AccountsRepository accountsRepository, AccountNumberGenerator accountNumberGenerator) {
+    @Autowired
+    public AccountsService(AccountsRepository accountsRepository, @Qualifier("fakeAccountNumberGenerator") AccountNumberGenerator accountNumberGenerator) {
         this.accountsRepository = accountsRepository;
         this.accountNumberGenerator = accountNumberGenerator;
     }
@@ -32,10 +42,12 @@ public class AccountsService {
         return new ArrayList<>();
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("AccountsService init...");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("AccountsService destroy...");
     }
