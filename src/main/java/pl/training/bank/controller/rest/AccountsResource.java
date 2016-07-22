4b7c99ca4334ecdb5278c1b15049f5261c9953ec
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2016-07-22 10:10:59 +0200

    base-mvc-javaconfig

--- src/main/java/pl/training/bank/controller/rest/AccountsResource.java
+++ src/main/java/pl/training/bank/controller/rest/AccountsResource.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pl.training.bank.dto.AccountDto;
+import pl.training.bank.dto.AccountsListDto;
 import pl.training.bank.dto.DtoMapper;
 import pl.training.bank.entity.Account;
 import pl.training.bank.service.AccountsService;
+import pl.training.bank.service.repository.ResultPage;
+
 import java.net.URI;
+import java.util.List;
 
 @RequestMapping(value = "api/accounts")
 @RestController
@@ -29,6 +35,15 @@ public class AccountsResource {
         Account account = accountsService.createAccount();
         URI uri = createUriForId(account.getId());
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public AccountsListDto getAccounts(
+            @RequestParam(required = false, defaultValue = "0", name = "pageNumber") int pageNumber,
+            @RequestParam(required = false, defaultValue = "10", name = "pageSize") int pageSize) {
+        ResultPage<Account> resultPage = accountsService.getAccounts(pageNumber, pageSize);
+        List<AccountDto> accountDtos = dtoMapper.map(resultPage.getContent(), AccountDto.class);
+        return new AccountsListDto(accountDtos, resultPage.getPageNumber(), resultPage.getTotalPages());
     }
 
     private URI createUriForId(long id) {
